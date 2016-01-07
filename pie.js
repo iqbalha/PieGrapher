1c3bcc55364732b659d53639f324c633ec2d758a
Nim Jayawardena <nim.jayawardena@mail.utoronto.ca>
2016-01-07 15:07:26 -0500

    Created pie.js

---
+++ pie.js
@@ -0,0 +1,11 @@
+/* ---- Entry Class ----- */
+class Entry () {
+	
+	constructor(name) {
+    this.name = name;			// Name.
+    this.color = color;			// Colour in hex.
+    this.quantity = quantity;	// Quanity.
+  }
+  
+}
+
