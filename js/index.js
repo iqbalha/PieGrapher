1d4bea7607579b4f8592be365179366013d094b0
Haris Iqbal <miniman.haris@gmail.com>
2016-01-07 16:36:02 -0500

    Java script code outline created

---
+++ js/index.js
@@ -0,0 +1,28 @@
+"use strict"
+
+// List of all the entry's
+entries = []
+
+/** 
+  Adds a new entry to the entries list. New entries will have a randomly
+  generated colour. Also add an extra row to the input table. 
+**/   
+function addNewEntry() {
+	
+}
+
+/** 
+  Deletes an existing entry from the entries list by index.
+  Remove the entries row from the input table.
+**/
+function deleteEntry(index) {
+	
+}
+
+/**
+  Clears the input table and entries list. Will also set the chart section
+  to go back to it's default apperance showing the instructions image.
+**/
+ function clear() {
+	
+}
